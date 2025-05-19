a398650a5b0317126b39fe943899c4bc00e0da8b
danifc123 <danielcarmo29@gmail.com>
2025-05-19 15:35:24 -0300

    updated: organizando a arquitetura do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutAuthComponent } from "./layouts/layout-auth/layout-auth.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutAuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/layouts/layout-auth/layout-auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-auth',
+  imports: [],
+  templateUrl: './layout-auth.component.html',
+  styleUrl: './layout-auth.component.scss'
+})
+export class LayoutAuthComponent {
+
+}
