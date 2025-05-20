8c2385de714176292c5b237affecd83bee15c7c4
danifc123 <danielcarmo29@gmail.com>
2025-05-20 16:23:57 -0300

    Updatred: new esqueci a senha and cadastrar and routerlink is configurated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LayoutAuthComponent } from './layouts/layout-auth/layout-auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+  path:"",
+  component: LayoutAuthComponent
+},
+{
+path:"cadastro",
+},
+{}];
