4b2a37e56af696d460ca7457d2cebe784bfb7308
danifc123 <danielcarmo29@gmail.com>
2025-05-20 15:32:55 -0300

    Fix: Implementing inputs to email and password

--- src/app/layouts/layout-auth/layout-auth.component.ts
+++ src/app/layouts/layout-auth/layout-auth.component.ts
@@ -1,9 +1,10 @@
 import { Component, viewChild, ViewEncapsulation } from '@angular/core';
 import { InputComponent } from "../../componentes/input/input.component";
+import { ButtonComponent } from "../../componentes/button/button.component";
 
 @Component({
   selector: 'app-layout-auth',
-  imports: [InputComponent],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './layout-auth.component.html',
   styleUrl: './layout-auth.component.scss',
   encapsulation:  ViewEncapsulation.None,
