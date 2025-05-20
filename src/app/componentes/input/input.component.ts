c91019c3b80bc349ebea75854f4ca397aa119e36
danifc123 <danielcarmo29@gmail.com>
2025-05-20 14:53:46 -0300

    Fix: criando o input e adicionando caracteristicas a ele

--- src/app/componentes/input/input.component.ts
+++ src/app/componentes/input/input.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   imports: [],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrl: './input.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class InputComponent {
-
+@Input() label : string = '';
+@Input() type : string = 'text';
 }
