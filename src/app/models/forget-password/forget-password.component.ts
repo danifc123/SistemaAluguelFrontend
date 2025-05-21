ecc084e7d6ad2557afc2234ede24b60ca5a7a771
danifc123 <danielcarmo29@gmail.com>
2025-05-21 15:13:23 -0300

    Fix: updated layout fotget password- fgt

--- src/app/models/forget-password/forget-password.component.ts
+++ src/app/models/forget-password/forget-password.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { InputComponent } from "../../componentes/input/input.component";
+import { ButtonComponent } from "../../componentes/button/button.component";
 
 @Component({
   selector: 'app-forget-password',
-  imports: [],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss'
 })
