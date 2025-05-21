0c55f70f1ac3a9ae67e67c819619af4f2e88ddb4
danifc123 <danielcarmo29@gmail.com>
2025-05-21 16:49:50 -0300

    added icon menu

--- src/app/layouts/layout-default/layout-default.component.ts
+++ src/app/layouts/layout-default/layout-default.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout-default',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './layout-default.component.html',
   styleUrl: './layout-default.component.scss'
 })
