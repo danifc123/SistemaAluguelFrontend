b7118f8f5e76c01e5ffc97ce547c9422fa705c5f
danifc123 <danielcarmo29@gmail.com>
2025-05-22 10:44:01 -0300

    fix: menu in layout

--- src/app/layouts/layout-default/layout-default.component.ts
+++ src/app/layouts/layout-default/layout-default.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "../../componentes/menu/menu.component";
 
 @Component({
   selector: 'app-layout-default',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './layout-default.component.html',
   styleUrl: './layout-default.component.scss'
 })
