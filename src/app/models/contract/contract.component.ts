4cdc7a60646c3ed71ae8449383b947f874bbb1a2
danifc123 <danielcarmo29@gmail.com>
2025-05-26 15:20:00 -0300

    Update: select layout editting

--- src/app/models/contract/contract.component.ts
+++ src/app/models/contract/contract.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../componentes/button/button.component";
+import { SelectComponent } from "../../componentes/select/select.component";
 import { InputComponent } from "../../componentes/input/input.component";
 
 @Component({
   selector: 'app-contract',
-  imports: [ButtonComponent, InputComponent],
+  imports: [ButtonComponent, SelectComponent, InputComponent],
   templateUrl: './contract.component.html',
   styleUrl: './contract.component.scss'
 })
