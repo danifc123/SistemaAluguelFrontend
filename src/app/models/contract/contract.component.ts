59dc554dc4b289de98b25a31eaab7d35ed861aab
danifc123 <danielcarmo29@gmail.com>
2025-05-26 10:27:01 -0300

    Updated: creating new component "contract"

--- src/app/models/contract/contract.component.ts
+++ src/app/models/contract/contract.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../componentes/button/button.component";
+import { InputComponent } from "../../componentes/input/input.component";
 
 @Component({
   selector: 'app-contract',
-  imports: [],
+  imports: [ButtonComponent, InputComponent],
   templateUrl: './contract.component.html',
   styleUrl: './contract.component.scss'
 })
