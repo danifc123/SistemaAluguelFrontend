8296ad5369f71af8e0c5fcc9a69b9939a1f3651b
danifc123 <danielcarmo29@gmail.com>
2025-05-29 09:10:10 -0300

    add onclic no button

--- src/app/models/contract/contract.component.ts
+++ src/app/models/contract/contract.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../componentes/button/button.component";
 import { SelectComponent } from "../../componentes/select/select.component";
+import { inquilinos } from '../../interface/inquilinos.model';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+
 
 @Component({
   selector: 'app-contract',
